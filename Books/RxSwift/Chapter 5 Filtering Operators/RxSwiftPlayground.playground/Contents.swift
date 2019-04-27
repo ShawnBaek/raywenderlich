69fa3ec1654e7c2e3f13bb8afab9ac4fb7a66c2b
ShawnBaek <shawn@shawnbaek.com>
2019-04-27 15:46:00 +0900

    Practice Chapter 5

---
+++ Books/RxSwift/Chapter 5 Filtering Operators/RxSwiftPlayground.playground/Contents.swift
@@ -0,0 +1,279 @@
+import Foundation
+import RxSwift
+
+example(of: "ignoreElements") {
+    let strikes = PublishSubject<String>()
+
+    let disposeBag = DisposeBag()
+    strikes
+        .ignoreElements()
+        .subscribe { _ in
+            print("You're out!")
+        }
+        .disposed(by: disposeBag)
+
+    strikes.onNext("X")
+    strikes.onNext("X")
+    strikes.onNext("X")
+
+    strikes.onCompleted()
+}
+
+example(of: "elementAt") {
+    let strikes = PublishSubject<String>()
+
+    let disposeBag = DisposeBag()
+
+    strikes
+        .elementAt(2)
+        .subscribe(onNext: { _ in
+            print("You're out!")
+        })
+        .disposed(by: disposeBag)
+
+    strikes.onNext("1")
+    strikes.onNext("2")
+    strikes.onNext("3")
+}
+
+example(of: "filter") {
+    let disposeBag = DisposeBag()
+
+    Observable.of(1, 2, 3, 4, 5, 6)
+        .filter { $0 % 2 == 0}
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+}
+
+example(of: "skip") {
+    let disposeBag = DisposeBag()
+
+    Observable.of("A", "B", "C", "D", "E", "F")
+        .skip(3)
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+}
+
+example(of: "skipWhile") {
+    let disposeBag = DisposeBag()
+
+    Observable.of(2, 2, 3, 4, 4)
+        .skipWhile { $0 % 2 == 0 }
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+}
+
+example(of: "skipUntil") {
+    let disposeBag = DisposeBag()
+
+    /// 1
+    let subject = PublishSubject<String>()
+    let trigger = PublishSubject<String>()
+
+    // 2
+    subject
+        .skipUntil(trigger)
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+
+    subject.onNext("A")
+    subject.onNext("B")
+
+    trigger.onNext("X")
+    subject.onNext("C")
+}
+
+example(of: "take") {
+    let disposeBag = DisposeBag()
+
+    /// 1
+    Observable.of(1, 2, 3, 4, 5, 6)
+        .take(3)
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+
+}
+
+example(of: "takeWhile") {
+    let disposeBag = DisposeBag()
+
+    // Create an observable of integers
+    Observable.of(2, 2, 4, 4, 6, 6)
+        .enumerated()
+        //destructure the tuple into individual arguments
+        .takeWhile { index, integer in
+            integer % 2 == 0 && index < 3
+        }
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+}
+
+example(of: "takeUntil") {
+    let disposeBag = DisposeBag()
+
+    // Create an observable of integers
+    let subject = PublishSubject<String>()
+    let trigger = PublishSubject<String>()
+
+    subject
+        .takeUntil(trigger)
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+
+    subject.onNext("1")
+    subject.onNext("2")
+
+    trigger.onNext("X")
+
+    subject.onNext("3")
+}
+
+example(of: "distinctUntilChanged") {
+    let disposeBag = DisposeBag()
+
+    Observable.of("A", "A", "B", "B", "A")
+        .distinctUntilChanged()
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+}
+
+example(of: "distinctUntilChanged(_:)") {
+    let disposeBag = DisposeBag()
+
+    // Create a number formatter to spell out each number
+    let formatter = NumberFormatter()
+    // 10 -> ten
+    formatter.numberStyle = .spellOut
+
+    //Carete an observable of NSNumbers(You don't have to convert integers when using the formatter next)
+    Observable<NSNumber>.of(10, 110, 20, 200, 210, 310)
+        .distinctUntilChanged { a, b in
+            guard let aWords = formatter.string(from: a)?.components(separatedBy: " "),
+                let bWords = formatter.string(from: b)?.components(separatedBy: " ") else {
+                    return false
+                }
+            var containsMatch = false
+            for aWord in aWords where bWords.contains(aWord) {
+                containsMatch = true
+                break
+            }
+            return containsMatch
+        }
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+}
+
+
+example(of: "Challenge 1") {
+    let disposeBag = DisposeBag()
+
+    let contacts = [
+        "603-555-1212": "Florent",
+        "212-555-1212": "Junior",
+        "408-555-1212": "Marin",
+        "617-555-1212": "Scott"
+    ]
+
+    func phoneNumber(from inputs: [Int]) -> String {
+        var phone = inputs.map(String.init).joined()
+
+        print("PhoneNumber: \(phone)")
+        phone.insert("-", at: phone.index(
+            phone.startIndex,
+            offsetBy: 3)
+        )
+
+        phone.insert("-", at: phone.index(
+            phone.startIndex,
+            offsetBy: 7)
+        )
+
+        return phone
+    }
+
+    let input = PublishSubject<Int>()
+
+    // Add your code here
+    input
+        .skipWhile { $0 == 0 }
+        .filter { $0 < 10 }
+        .take(10)
+        .toArray()
+        .subscribe(onNext: {
+            let phone = phoneNumber(from: $0)
+            //217-555-1212
+            print("First : \(phone)")
+            if let contact = contacts[phone] {
+                print("Dialing \(contact) (\(phone))...")
+            } else {
+                print("Contact not found")
+            }
+        })
+        .disposed(by: disposeBag)
+
+    input.onNext(0)
+    input.onNext(603)
+
+    input.onNext(2)
+    input.onNext(1)
+
+    // Confirm that 7 results in "Contact not found", and then change to 2 and confirm that Junior is found
+    input.onNext(2)
+
+    "5551212".forEach {
+        if let number = (Int("\($0)")) {
+            print("number: \(number)")
+            input.onNext(number)
+        }
+    }
+
+    input.onNext(9)
+}
+
+/*:
+ Copyright (c) 2019 Razeware LLC
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ distribute, sublicense, create a derivative work, and/or sell copies of the
+ Software in any work that is designed, intended, or marketed for pedagogical or
+ instructional purposes related to programming, coding, application development,
+ or information technology.  Permission for such use, copying, modification,
+ merger, publication, distribution, sublicensing, creation of derivative works,
+ or sale is expressly withheld.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
